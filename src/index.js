f634f103a6a2b076c3442cc9ba9329331014fa06
tejaswipandava <tejaswi.pandava@gmail.com>
2026-02-21 18:42:18 +0530

    redux reducer and store

--- src/index.js
+++ src/index.js
@@ -5,12 +5,30 @@ import {
   bindActionCreators,
 } from "redux";
 
-const toUpper = (string) => string.toUpperCase();
-const toRepeat = (string) => string.repeat(3);
-const toBold = (string) => string.bold();
+// const toUpper = (string) => string.toUpperCase();
+// const toRepeat = (string) => string.repeat(3);
+// const toBold = (string) => string.bold();
 
-const mutant = (string) => toBold(toRepeat(toUpper(string)));
-const composedmutant = compose(toBold, toRepeat, toUpper);
+// const mutant = (string) => toBold(toRepeat(toUpper(string)));
+// const composedmutant = compose(toBold, toRepeat, toUpper);
 
-console.log(mutant("hello"));
-console.log(composedmutant("jaffa"));
+// console.log(mutant("hello"));
+// console.log(composedmutant("jaffa"));
+
+
+//redux store, reducer----------------------------------------
+const initialState = { value: 10 };
+
+const INCREMENT = "INCREMENT";
+const incrementAction = { type: INCREMENT };
+
+const reducer = (state, action) => {
+  if (action.type === INCREMENT) {
+    return { state: state.value + 1 };
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+
+console.log(store);
