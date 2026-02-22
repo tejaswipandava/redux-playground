3276b1b59761807ea49c426300685bc1084be021
tejaswipandava <tejaswi.pandava@gmail.com>
2026-02-22 20:13:43 +0530

    middleware
    middleware are safer cousins to apply middleware like logging or delaying dispatcher and so on

--- src/index.js
+++ src/index.js
@@ -8,37 +8,61 @@ import {
 
 const reducer = (state) => state;
 
-const monitorEnhancer = (createStore) => (reducer, initialState, enhance) => {
-  const monitorReducer = (state, action) => {
-    const start = performance.now();
-    const newState = reducer(state, action);
-    const end = performance.now();
-    const diff = end - start;
-    console.log("difference in time", diff);
+// const monitorEnhancer = (createStore) => (reducer, initialState, enhance) => {
+//   const monitorReducer = (state, action) => {
+//     // Ignore Redux internal actions
+//     if (action.type.startsWith("@@redux/")) {
+//       return;
+//     }
+//     const start = performance.now();
+//     const newState = reducer(state, action);
+//     const end = performance.now();
+//     const diff = end - start;
+//     console.log("difference in time", diff);
 
-    return newState;
-  };
+//     return newState;
+//   };
 
-  return createStore(monitorReducer, initialState, enhance);
+//   return createStore(monitorReducer, initialState, enhance);
+// };
+
+// const logEnhancer = (createStore) => (reducer, initialState, enhance) => {
+//   const logReducer = (state, action) => {
+//     // Ignore Redux internal actions
+//     if (action.type.startsWith("@@redux/")) {
+//       return reducer(state, action);
+//     }
+
+//     console.log("old state", state, "action: ", action);
+//     const newState = reducer(state + 1, action);
+//     console.log("new state", newState, "action: ", action);
+
+//     return newState;
+//   };
+
+//   return createStore(logReducer, initialState, enhance);
+// };
+
+//middle ware
+const logMiddleware = (store) => (next) => (action) => {
+  console.log("old state", store.getState(), "action: ", action);
+  next(action);
+  console.log("new state", store.getState(), "action: ", action);
 };
 
-const logEnhancer = (createStore) => (reducer, initialState, enhance) => {
-  const logReducer = (state, action) => {
-    // Ignore Redux internal actions
-    if (action.type.startsWith("@@redux/")) {
-      return reducer(state, action);
-    }
-
-    console.log("old state", state, "action: ", action);
-    const newState = reducer(state + 1, action);
-    console.log("new state", newState, "action: ", action);
-
-    return newState;
-  };
-
-  return createStore(logReducer, initialState, enhance);
+const monitorMiddleware = (store) => (next) => (action) => {
+  const start = performance.now();
+  next(action);
+  const end = performance.now();
+  const diff = end - start;
+  console.log("difference in time", diff);
 };
-const store = createStore(reducer, 1, compose(logEnhancer));
+
+const store = createStore(
+  reducer,
+  1,
+  applyMiddleware(logMiddleware, monitorMiddleware)
+);
 console.log("trigger");
 store.dispatch({ type: "test" });
 console.log(store.getState());
